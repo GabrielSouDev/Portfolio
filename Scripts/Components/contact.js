78bd56d14328f19c6c6313ec205fd0a791a7f2d5
Gabriel Souza <61925245+GabrielSouDev@users.noreply.github.com>
2025-05-29 20:00:44 -0300

    mecanismo de envio de email contato alterado para email.js

--- Scripts/Components/contact.js
+++ Scripts/Components/contact.js
@@ -1,23 +1,24 @@
 export const init = () => {
-    document.getElementById("email-form").addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        const url = "https://formsubmit.co/gabriel.calde21@gmail.com";
-        const formData = new FormData(this);
+  emailjs.init({
+    publicKey: 'pT5g-klgLvL1isc7',
+  });
+
+  const form = document.getElementById("email-form");
+  const successMessage = document.getElementById("success-message");
+  const errorMessage = document.getElementById("error-message");
     
-        fetch(url, {
-          method: "POST",
-          body: formData
-        })
-        .then(response => {
-          if (response.ok) {
-            alert("Mensagem enviada com sucesso!");
-          } else {
-            alert("Erro ao enviar a mensagem.");
-          }
-        })
-        .catch(error => {
-          alert("Erro ao enviar: " + error.message);
-        });
-      });
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+        
+    emailjs.sendForm('service_02n53j9', 'template_pkma8ts', this)
+    .then(() => {
+      successMessage.style.display = 'block';
+      errorMessage.style.display = 'none';
+      form.reset();
+    }, (error) => {
+      successMessage.style.display = 'none';
+      errorMessage.style.display = 'block';
+      console.error('Erro:', error);
+    });
+  });
 };
